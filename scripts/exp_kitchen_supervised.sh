394ee32a85d64e6f4669a5b9c496926db7ca34fd
Liuba Orlova <liuba1156@gmail.com>
2025-04-07 22:03:14 -0500

    Add kitchen bash scripts

---
+++ scripts/exp_kitchen_supervised.sh
@@ -0,0 +1,5 @@
+python3 train.py --config-name=train_kitchen student_type=supervised seed=0 project=neurips25_kitchen batch_size=64
+python3 train.py --config-name=train_kitchen student_type=supervised seed=1 project=neurips25_kitchen batch_size=64
+python3 train.py --config-name=train_kitchen student_type=supervised seed=2 project=neurips25_kitchen batch_size=64
+python3 train.py --config-name=train_kitchen student_type=supervised seed=3 project=neurips25_kitchen batch_size=64
+python3 train.py --config-name=train_kitchen student_type=supervised seed=4 project=neurips25_kitchen batch_size=64
---
+++ scripts/exp_kitchen_unsupervised.sh
@@ -0,0 +1,5 @@
+python3 train.py --config-name=train_kitchen student_type=unsupervised seed=0 project=neurips25_kitchen batch_size=64
+python3 train.py --config-name=train_kitchen student_type=unsupervised seed=1 project=neurips25_kitchen batch_size=64
+python3 train.py --config-name=train_kitchen student_type=unsupervised seed=2 project=neurips25_kitchen batch_size=64
+python3 train.py --config-name=train_kitchen student_type=unsupervised seed=3 project=neurips25_kitchen batch_size=64
+python3 train.py --config-name=train_kitchen student_type=unsupervised seed=4 project=neurips25_kitchen batch_size=64
