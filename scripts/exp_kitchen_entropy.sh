1f5f7fb0ad2c05a3d407cc5ae828117de9893014
Liuba Orlova <liuba1156@gmail.com>
2025-05-05 19:53:27 -0500

    30% budget scripts

--- scripts/exp_kitchen_entropy.sh
+++ scripts/exp_kitchen_entropy.sh
@@ -1,10 +1,10 @@
 echo 'First experiment latent entropy'
-python3 train.py --config-name=train_kitchen student_type=latent_entropy_based seed=0 project=neurips25_kitchen_20percent batch_size=64 query_percentage_budget=0.2
+python3 train.py --config-name=train_kitchen student_type=latent_entropy_based seed=0 project=neurips25_kitchen_30percent batch_size=64 query_percentage_budget=0.3
 echo 'Second experiment latent entropy'
-python3 train.py --config-name=train_kitchen student_type=latent_entropy_based seed=1 project=neurips25_kitchen_20percent batch_size=64 query_percentage_budget=0.2
+python3 train.py --config-name=train_kitchen student_type=latent_entropy_based seed=1 project=neurips25_kitchen_30percent batch_size=64 query_percentage_budget=0.3
 echo 'Third experiment latent entropy'
-python3 train.py --config-name=train_kitchen student_type=latent_entropy_based seed=2 project=neurips25_kitchen_20percent batch_size=64 query_percentage_budget=0.2
+python3 train.py --config-name=train_kitchen student_type=latent_entropy_based seed=2 project=neurips25_kitchen_30percent batch_size=64 query_percentage_budget=0.3
 echo 'Fourth experiment latent entropy'
-python3 train.py --config-name=train_kitchen student_type=latent_entropy_based seed=3 project=neurips25_kitchen_20percent batch_size=64 query_percentage_budget=0.2
+python3 train.py --config-name=train_kitchen student_type=latent_entropy_based seed=3 project=neurips25_kitchen_30percent batch_size=64 query_percentage_budget=0.3
 echo 'Fifth experiment latent entropy'
-python3 train.py --config-name=train_kitchen student_type=latent_entropy_based seed=4 project=neurips25_kitchen_20percent batch_size=64 query_percentage_budget=0.2
+python3 train.py --config-name=train_kitchen student_type=latent_entropy_based seed=4 project=neurips25_kitchen_30percent batch_size=64 query_percentage_budget=0.3
