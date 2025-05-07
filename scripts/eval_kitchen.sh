bff4b6fa5789ea86a8530aaee2ceac1a130239f3
Liuba Orlova <liuba1156@gmail.com>
2025-05-07 14:40:50 -0500

    Add evals 20%

---
+++ scripts/eval_kitchen.sh
@@ -0,0 +1,5 @@
+python3 run_on_env.py --config-name=eval_kitchen env.load_dir=$(pwd)/exp_local/2025.05.06/004423_kitchen_train
+python3 run_on_env.py --config-name=eval_kitchen env.load_dir=$(pwd)/exp_local/2025.05.06/005148_kitchen_train
+python3 run_on_env.py --config-name=eval_kitchen env.load_dir=$(pwd)/exp_local/2025.05.06/065333_kitchen_train
+python3 run_on_env.py --config-name=eval_kitchen env.load_dir=$(pwd)/exp_local/2025.05.06/070631_kitchen_train
+python3 run_on_env.py --config-name=eval_kitchen env.load_dir=$(pwd)/exp_local/2025.05.06/130316_kitchen_train
---
+++ scripts/eval_kitchen2.sh
@@ -0,0 +1,5 @@
+python3 run_on_env.py --config-name=eval_kitchen env.load_dir=$(pwd)/exp_local/2025.05.06/132113_kitchen_train
+python3 run_on_env.py --config-name=eval_kitchen env.load_dir=$(pwd)/exp_local/2025.05.06/191337_kitchen_train
+python3 run_on_env.py --config-name=eval_kitchen env.load_dir=$(pwd)/exp_local/2025.05.06/194503_kitchen_train
+python3 run_on_env.py --config-name=eval_kitchen env.load_dir=$(pwd)/exp_local/2025.05.07/021055_kitchen_train
+python3 run_on_env.py --config-name=eval_kitchen env.load_dir=$(pwd)/exp_local/2025.05.07/012459_kitchen_train
